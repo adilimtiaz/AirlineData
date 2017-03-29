6443046da846d152d06e57174684aa74790e9e71
Andrew Trinh <Trinh@dhcp-128-189-86-118.ubcsecure.wireless.ubc.ca>
2017-03-29 15:41:01 -0700

    no message

--- admin.php
+++ admin.php
@@ -3,48 +3,6 @@
 
 <table><tr valign="top"><td width="40%">
 
-<div id="editTicket">
-    <h2>Edit a Reservation</h2>
-    Leave fields blank to keep current value
-    <form method="POST" action= <?php __FILE__ ?> >
-        <table>
-	    <tr>
-                <td>Ticket ID</td>
-                <td><input type="input" name="tid" required></td>
-            </tr>
-	    <tr>
-                <td>Flight No.</td>
-                <td><input type="input" name="fno"></td>
-            </tr>
-	    <tr>
-                <td>Depatrure Date</td>
-                <td><input type="date" name="dateflight"></td>
-            </tr>
-	    <tr>
-                <td>Passenger ID</td>
-                <td><input type="input" name="pid"></td>
-            </tr>
-            <tr>
-                <td><button type="submit" value="submit" name="editResSubmit">Edit</button></td>
-            </tr>
-        </table>
-    </form>
-</div>
-
-<div id="removeTicket">
-    <h2>Remove a Reservation</h2>
-    <form method="POST" action= <?php __FILE__ ?> >
-        <table>
-	    <tr>
-                <td>Ticket ID</td>
-                <td><input type="input" name="tid" required></td>
-            </tr>
-            <tr>
-                <td><button type="submit" value="submit" name="removeResSubmit">Remove</button></td>
-            </tr>
-        </table>
-    </form>
-</div>
 
 <div id="editCapacity">
     <h2>Edit Model Capacity</h2>
@@ -80,37 +38,6 @@
     </form>
 </div>
 
-<div id="editFlightTimes">
-    <h2>Edit a Flight</h2>
-    Leave fields blank to keep current value
-    <form method="POST" action= <?php __FILE__ ?> >
-        <table>
-	    <tr>
-                <td>Flight No.</td>
-                <td><input type="input" name="fno" required></td>
-            </tr>
-	    <tr>
-                <td>Depatrure Date</td>
-                <td><input type="date" name="dateflight" required></td>
-            </tr>
-	    <tr>
-                <td>Depatrure Time</td>
-                <td><input type="time" name="deptime"></td>
-            </tr>
-	    <tr>
-                <td>Arrival Time</td>
-                <td><input type="time" name="arrtime"></td>
-            </tr>
-	    <tr>
-                <td>Aircraft ID</td>
-                <td><input type="input" name="regno"></td>
-            </tr>
-            <tr>
-                <td><button type="submit" value="submit" name="editFlightSubmit">Edit</button></td>
-            </tr>
-        </table>
-    </form>
-</div>
 
 <div id="addFlight">
     <h2>Add a Flight</h2>
@@ -210,6 +137,22 @@
         $query = "update modelinfo set capacity = $capacity where model = $model";
 
         $print = false;
+    }
+
+    if (array_key_exists('addFlightSubmit', $_POST)) {
+    	$fno = "'".$_POST['fno']."'";
+    	$dateflight = "'".strtoupper(date('Y-m-d', strtotime($_POST['dateflight'])))."'";
+    	$deptime = "'".$_POST['deptime']."'";
+    	$arrtime = "'".$_POST['arrtime']."'";
+    	$depacode = "'".$_POST['depacode']."'";
+    	$arracode = "'".$_POST['arracode']."'";
+    	$arrdate = "'".strtoupper(date('Y-m-d', strtotime($_POST['arrdate'])))."'";
+    	$regno = "'".$_POST['regno']."'";
+
+
+
+    	$query = "insert into flight values($fno, $dateflight, $deptime, $depacode, $arrdate, $arrtime, $arracode,$regno)";
+
     }
 
     if (array_key_exists('viewModelInfo', $_POST)) {
